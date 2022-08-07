981b3acf20f841f52b58676fa7d0bf287a71a660
deehpapa <andrepgimenes@live.com>
2022-08-07 03:39:44 -0300

    Aula 03 - Final

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,31 @@
-// https://youtu.be/JGlDpIYk6zU?t=1993
-window.alert('Ola!')
+const btnNext = document.getElementById("btn-next");
+const btnBack = document.getElementById("btn-back");
+const cards = document.querySelectorAll(".card");
+
+let cardNow = 0;
+
+function hideSelectedCard(){
+    const cardSelected = document.querySelector(".choose");
+    cardSelected.classList.remove("choose");
+}
+
+function presentSelectedCard(indexCard){
+    cards[indexCard].classList.add("choose");
+}
+
+btnNext.addEventListener("click", function (){
+    if(cardNow === cards.length - 1){
+        cardNow = -1;
+    }
+    hideSelectedCard();
+    cardNow++;
+    presentSelectedCard(cardNow);
+});
+
+btnBack.addEventListener("click", function (){
+    if(cardNow === 0) return;
+
+    hideSelectedCard();
+    cardNow--;
+    presentSelectedCard(cardNow);
+});
